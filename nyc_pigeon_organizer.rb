1da3f3fbefb83ec3ae3312b3afc341283e52f768
Chip Lempke <clempke90@gmail.com>
2020-12-08 15:31:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,13 @@ def nyc_pigeon_organizer(data)
   final = data.each_with_object({}) do |(key, value), new_array|
     value.each do |inner_key, names|
       names.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
+        if !new_array[name]
+          new_array[name] = {}
         end
-        if !final_array[name][key]
-          !final_array[name][key] = []
+        if !new_array[name][key]
+          !new_array[name][key] = []
         end
-        final_array[name][key].push(inner_key.to_s)
+        new_array[name][key].push(inner_key.to_s)
       end
     end
     new_array
