548a1d575cb4dbbeb4ffa5e05c734c45fe5b04d4
Chip Lempke <clempke90@gmail.com>
2020-12-08 15:20:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(keys, value), new_array|
     value.each do |inner_key, names|
-      names.each do |names|
+      names.each do |name|
     end
     new_array
   end
